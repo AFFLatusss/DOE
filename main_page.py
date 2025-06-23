bfbbbd9b4efc9ae348c6c24ec32aea4a2ae967bb
AFFLatusss <jasonyee1999@gmail.com>
2025-06-23 10:39:32 +0800

    radio

--- main_page.py
+++ main_page.py
@@ -3,3 +3,14 @@ import streamlit as st
 # Main page content
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
+
+doe_type = st.radio("Select DOE type", 
+                 ["Full Factorial Design(General)", "Full Factorial Design(2-Level)",
+                  "Fractional Factorial Design", "Plackett-Burman Design",
+                  "Box-Behnken Design", "Central Composite Design", "Mixture Design "],
+                 index=0)
+
+
+
+
+st.write("You selected:", doe_type)
--- page_2.py
+++ page_2.py
@@ -2,10 +2,16 @@ import streamlit as st
 import pandas as pd
 
 
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
+st.markdown("# DOE Analyse")
+# data = st.file_uploader("Upload a CSV file", type=["csv"])
 
-data = st.file_uploader("Upload a CSV file", type=["csv"])
+st.set_page_config(page_title="DOE Analyse", layout="wide")
+
+st.sidebar.header("🛠 Setup")
+
+# Option to upload CSV
+st.sidebar.subheader("Upload Factor Data (optional)")
+data = st.sidebar.file_uploader("Choose a CSV file", type="csv")
 
 if data:
     df = pd.read_csv(data)
@@ -21,3 +27,5 @@ if data:
     # st.write("You selected:", factors)
 
     st.selectbox('Select a response variable', options=[x for x in columns if x not in factors], index=0)
+else:
+    st.info("No CSV uploaded. Please upload a CSV to proceed. Format: one column per factor, no response column.")
