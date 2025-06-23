8a7ebb3a8d848fce623fa54d758d1f50afed46dd
AFFLatusss <jasonyee1999@gmail.com>
2025-06-23 09:50:25 +0800

    design

--- page_2.py
+++ page_2.py
@@ -10,3 +10,14 @@ data = st.file_uploader("Upload a CSV file", type=["csv"])
 if data:
     df = pd.read_csv(data)
     st.dataframe(df)
+
+    columns = df.columns.tolist() if data else []
+    factors = st.multiselect(
+        "Select Factors?",
+        columns,
+        default=[],
+    )
+
+    # st.write("You selected:", factors)
+
+    st.selectbox('Select a response variable', options=[x for x in columns if x not in factors], index=0)
